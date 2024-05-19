57cebf52e315da912ebc781a3fa0b436a0cd2a76
AsAnder9934 <149382112+AsAnder9934@users.noreply.github.com>
2024-05-19 12:39:34 +0200

    fix_1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Home from "./components/home/Home";
 import Services from "./components/services/Services";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Map from "./components/map/Map";
-import { Dashboard } from "@mui/icons-material";
+// import { Dashboard } from "@mui/icons-material";
+import Dashboard from "./components/dashboard/Dashboard";
 
 const router = createBrowserRouter([
   {
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,13 +3,32 @@ import MediaCard from "./Card";
 
 function Dashboard() {
   const input_list = [
-    { name: "Janek", surname: "kowalski", content: "jakiś opis" },
-    { name: "maciek", surname: "Malinowski", content: "jakiś opis" },
+    {
+      name: "Janek",
+      surname: "kowalski",
+      content: "jakiś opis",
+      image:
+        "https://marcinbiodrowski.com/wp-content/uploads/2018/03/dlaczego-warto-miec-dobre-zdjecie-w-CV.jpg",
+    },
+
+    {
+      name: "maciek",
+      surname: "Malinowski",
+      content: "jakiś opis",
+      image:
+        "https://marcinbiodrowski.com/wp-content/uploads/2018/03/dlaczego-warto-miec-dobre-zdjecie-w-CV.jpg",
+    },
   ];
   return (
     <div>
       {input_list.map((item) => {
-        return <MediaCard name={item.name} surname={item.surname} />;
+        return (
+          <MediaCard
+            name={item.name}
+            surname={item.surname}
+            image={item.image}
+          />
+        );
       })}
       <MediaCard />
     </div>
