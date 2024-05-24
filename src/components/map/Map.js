729f48792ee9bc33fef2d1a6de442dc8303bc5e6
AsAnder9934 <149382112+AsAnder9934@users.noreply.github.com>
2024-05-24 11:20:11 +0200

    wip:add-api_menagement

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Map.css";
-import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
+import {
+  LayersControl,
+  MapContainer,
+  TileLayer,
+  WMSTileLayer,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import axios from "axios";
 
 function Map() {
+  useEffect(() => {
+    console.log("aaa");
+    const getData = () => {
+      axios
+        .get(
+          // "http://127.0.0.1:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3AA01_Granice_wojewodztw_db&maxFeatures=50&outputFormat=application%2Fjson"
+          "https://jsonplaceholder.typicode.com/posts/1"
+        )
+        .then((dane) => {
+          console.log(dane);
+        });
+    };
+    getData();
+  }, []);
+
   return (
     <div className="map">
-      <MapContainer center={[52.2322222, 21.0]} zoom={15}>
+      <MapContainer center={[52.2322222, 21.0]} zoom={6}>
         <LayersControl>
           <LayersControl.BaseLayer checked name="OSM">
             <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
@@ -17,6 +38,15 @@ function Map() {
           <LayersControl.BaseLayer checked name="Google Satelite">
             <TileLayer url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}" />
           </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer checked name="granice wojewodztw db">
+            <WMSTileLayer
+              layers="A01_Granice_wojewodztw_db"
+              url="http://127.0.0.1:8080/geoserver/prge/wms"
+            />
+          </LayersControl.BaseLayer>
+          {/* <LayersControl.Overlay checked name="granice wojewodztwa db WFS">
+            <GeoJSON data={A01_Granice_wojewodztw_db} />
+          </LayersControl.Overlay> */}
         </LayersControl>
       </MapContainer>
     </div>
