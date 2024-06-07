5ead2339f521ae25fdbe7c6012b18ba7e942acdd
AsAnder9934 <149382112+AsAnder9934@users.noreply.github.com>
2024-06-07 11:12:38 +0200

    modified:   src/components/map/Map.js

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,25 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Map.css";
 import {
   LayersControl,
   MapContainer,
   TileLayer,
   WMSTileLayer,
+  GeoJSON,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import axios from "axios";
 
 function Map() {
+  const [wojewodztwa, setWojewodztwa] = useState(null);
+
+  const makePopup = (feature, layer) => {
+    if (feature.properties) {
+      console.log(feature.properties.jpt_nazwa_);
+      layer.bindPopup(`
+        <h1> Dane województwa</h1>
+        
+        <strong>Województwo: </strong>${feature.properties.jpt_nazwa_}</br>
+        <strong>Powierzchnia: </strong>${feature.properties.jpt_powier}</br>
+        <img src=${feature.properties.img_source} alt="Lamp" width="32" height="32" />
+        `);
+    }
+  };
+
   useEffect(() => {
     console.log("aaa");
     const getData = () => {
       axios
         .get(
-          // "http://127.0.0.1:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3AA01_Granice_wojewodztw_db&maxFeatures=50&outputFormat=application%2Fjson"
-          "https://jsonplaceholder.typicode.com/posts/1"
+          "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3AA01_Granice_wojewodztw_db_4326&maxFeatures=50&outputFormat=application%2Fjson"
+          // "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3AA01_Granice_wojewodztw_db&maxFeatures=50&outputFormat=application%2Fjson"
+          //"https://jsonplaceholder.typicode.com/posts/1"
         )
         .then((dane) => {
           console.log(dane);
+          setWojewodztwa(dane.data);
         });
     };
     getData();
@@ -27,26 +45,30 @@ function Map() {
 
   return (
     <div className="map">
-      <MapContainer center={[52.2322222, 21.0]} zoom={6}>
+      <MapContainer center={[52.2322222, 21.0]} zoom={10}>
         <LayersControl>
           <LayersControl.BaseLayer checked name="OSM">
             <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer checked name="Google">
+          <LayersControl.BaseLayer name="Google">
             <TileLayer url="http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer checked name="Google Satelite">
+          <LayersControl.BaseLayer name="Google Satelite">
             <TileLayer url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer checked name="granice wojewodztw db">
+          <LayersControl.BaseLayer name="Granice województw DB">
             <WMSTileLayer
-              layers="A01_Granice_wojewodztw_db"
+              layers="granice_wojewodztw"
               url="http://127.0.0.1:8080/geoserver/prge/wms"
             />
           </LayersControl.BaseLayer>
-          {/* <LayersControl.Overlay checked name="granice wojewodztwa db WFS">
-            <GeoJSON data={A01_Granice_wojewodztw_db} />
-          </LayersControl.Overlay> */}
+          <LayersControl.Overlay checked name="Granice województw DB WFS">
+            {wojewodztwa ? (
+              <GeoJSON data={wojewodztwa} onEachFeature={makePopup} />
+            ) : (
+              ""
+            )}
+          </LayersControl.Overlay>
         </LayersControl>
       </MapContainer>
     </div>
